f12d12ca8af6cba359ca65c3c6ade6857cc0ea35
Dynamite <89369266+lom123ga@users.noreply.github.com>
2023-03-24 04:10:47 +0700

    Update 24/03/2023
    Complete View-LoginAndRegister

---
+++ src/main/java/MVC/view/LoginAndRegister/Swing/EventLogin.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package MVC.view.LoginAndRegister.Swing;
+
+/**
+ *
+ * @author ssopt
+ */
+public interface EventLogin {
+    public void loginDone();
+
+    public void logOut();
+}
